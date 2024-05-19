b3c9d9338db0c1f012f2ef9c4002f8841509cd4f
stephen_adah <mailstephenreal@gmail.com>
2024-05-19 10:01:38 +0100

    annotate fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ from typing import List
 import re
 
 
-def filter_datum(fields: [str], redaction: str, message: str, separator: str
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         ) -> str:
     """returns a log message obfuscated"""
     return re.sub(f'({"|".join(fields)})=[^;]*', f'\\1={redaction}', message)
