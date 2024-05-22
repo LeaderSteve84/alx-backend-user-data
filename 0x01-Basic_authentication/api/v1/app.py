299623c29eb2b7f0307a75aa18287d251c25aeca
stephen_adah <mailstephenreal@gmail.com>
2024-05-22 13:46:18 +0100

    task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+AUTH_TYPE = getenv("AUTH_TYPE")
+
+if AUTH_TYPE == auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -32,6 +38,24 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """handles forbidden error"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> str:
+    """to handler before_request"""
+    exclude_path = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+        ]
+
+    if auth is not None and \
+            auth.require_auth(request.path, exclude_paths) is True:
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,10 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object
         """
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None - request will be the Flask request object
