abd2b1760228eae8428d0efba98a8c745983c5af
stephen_adah <mailstephenreal@gmail.com>
2024-05-22 18:26:37 +0100

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """class BasicAuth"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+from typing import TypeVar, List
+from models.user import User
+from flask import request
 
 
 class BasicAuth(Auth):
@@ -17,3 +21,16 @@ class BasicAuth(Auth):
                     in str(authorization_header).split(" ")[0]:
                 return authorization_header.split(" ")[1]
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+    ) -> str:
+        """returns the decoded value of a Base64 string
+        base64_authorization_header:
+        """
+        try:
+            value_of_utf = base64_authorization_header.encode('utf-8')
+            decode_value= b64decode(value_of_utf).decode('utf-8')
+            return decode_value
+        except (AttributeError, ValueError) as e:
+            return None
