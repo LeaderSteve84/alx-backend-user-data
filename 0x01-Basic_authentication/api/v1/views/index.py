b76f90b072b9a81ed533517884448d2a7a4414a6
stephen_adah <mailstephenreal@gmail.com>
2024-05-22 10:32:10 +0100

    task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', strict_slashes=False, methods=['GET'])
+def unathorized() -> str:
+    """GET /api/v1/unauthorized"""
+    abort(401)
