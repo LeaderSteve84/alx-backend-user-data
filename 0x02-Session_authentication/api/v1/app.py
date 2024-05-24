4735d921e5b09955d40bf20fac7ec6f932688f0d
stephen_adah <mailstephenreal@gmail.com>
2024-05-24 17:57:29 +0100

    corection

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -60,8 +60,7 @@ def before_request() -> str:
     if auth is not None and \
             auth.require_auth(request.path, exclude_paths) is True:
 
-        if auth.authorization_header(request) is None and \
-                and auth.session_cookie(request) is None:
+        if auth.authorization_header(request) is None:
             abort(401)
 
         if auth.current_user(request) is None:
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,4 +5,3 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """class for session Authentication"""
-    pass
