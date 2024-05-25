a40998c29cb7298bcba47e0f5acd9731d2e47a4c
stephen_adah <mailstephenreal@gmail.com>
2024-05-25 09:56:21 +0100

    after debuging

--- 0x02-Session_authentication/api/v1/auth/debug_session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/debug_session_auth.py
@@ -19,6 +19,8 @@ class SessionAuth(Auth):
 
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
+        print(f"Created session ID: {session_id} for user ID: {user_id}")  # Debug statement
+        print(self.user_id_by_session_id)
 
         return session_id
 
@@ -33,6 +35,7 @@ class SessionAuth(Auth):
 
         else:
             user_id = self.user_id_by_session_id.get(session_id)
+            print(f"Retrieved user ID: {user_id} for session ID: {session_id}")  # Debug statement
             return user_id
 
     def session_cookie(self, request=None):
@@ -40,14 +43,17 @@ class SessionAuth(Auth):
         if request is None:
             return None
         cookie = request.cookies.get('_my_session_id')
+        print("Session cookie:", cookie)  # Debug statement
         return cookie
 
     def current_user(self, request=None):
         """returns a User instance based on a cookie value:"""
         session_id = self.session_cookie(request)
+        print("Session ID:", session_id)  # Debug statement
         if session_id is None:
             return None
         user_id = self.user_id_for_session_id(session_id)
+        print("User ID:", user_id)  # Debug statement
         if user_id:
             return User.get(user_id)
         return None
